bfeace2349634d5928141a6ec8ccd8780ef201e5
1g2g <wonji126326@gmail.com>
2023-03-09 15:39:10 +0900

    Post, update, delete Nweets

--- NWITTER/src/components/App.js
+++ NWITTER/src/components/App.js
@@ -4,21 +4,28 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      //로그인 상태가 변할 때
+      //로그인 상태가 변할 때 또는 어플리케이션 초기화 시 발생
       if (user) {
-        setIsLoggedIn(true);
+        // setIsLoggedIn(true);
+        setUserObj(user);
       } else {
-        setIsLoggedIn(false);
+        // setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
     });
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- NWITTER/src/fbase.js
+++ NWITTER/src/fbase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-
+import "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -14,4 +14,5 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
 //가장 많이 사용할 auth()를 한 번만 호출하게 하여 export함
